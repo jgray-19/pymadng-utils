6318543336381e0108f25ac3fa68d54fa9660dd1
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2026-02-25 13:36:00 +0100

    Update __all__ in __init__.py to include tfs_utils functions for better accessibility

--- src/pymadng_utils/madx/__init__.py
+++ src/pymadng_utils/madx/__init__.py
@@ -1,5 +1,13 @@
 from .make_sequence import make_madx_sequence
+from .tfs_utils import (
+    convert_multiple_tfs_files,
+    convert_tfs_to_madx,
+    export_tfs_to_madx,
+)
 
 __all__ = [
     "make_madx_sequence",
+    "convert_tfs_to_madx",
+    "export_tfs_to_madx",
+    "convert_multiple_tfs_files",
 ]
